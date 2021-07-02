afd805c64680b240efabbfc422b1cb84079332b8
Adam Becker <amkbecker@gmail.com>
2021-07-02 14:59:41 +0200

    Update xxx

--- src/js/script.js
+++ src/js/script.js
@@ -18,6 +18,7 @@
     },
     menuProduct: {
       clickable: '.product__header',
+      activeProduct: '.product.activ',
       form: '.product__order',
       priceElem: '.product__total-price .price',
       imageWrapper: '.product__images',
@@ -85,32 +86,32 @@
 
       
     initAccordion(){
-        debugger
+      //debugger
       const thisProduct = this;
 
-    /* find the clickable trigger (the element that should react to clicking) */
+      /* find the clickable trigger (the element that should react to clicking) */
         
       const clickableTrigger = document.querySelector(select.menuProduct.clickable);
 
-    /* START: add event listener to clickable trigger on event click */
+      /* START: add event listener to clickable trigger on event click */
     
       clickableTrigger.addEventListener('click', function(event) {
         
-      /* prevent default action for event */
+        /* prevent default action for event */
         
         event.preventDefault();
                 
-      /* find active product (product that has active class) */
+        /* find active product (product that has active class) */
 
-        const activeProduct = classNames.menuProduct.wrapperActive;
+        const activeProduct = document.querySelector(select.menuProduct.activeProduct);
         
-      /* if there is active product and it's not thisProduct.element, remove class active from it */
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
 
         if (activeProduct !== null && activeProduct !== thisProduct.element){
-          thisProduct.element.classList.remove(classNames.menuProduct.wrapperActive);    
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);    
         }
         
-      /* toggle active class on thisProduct.element */
+        /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
             
       });
